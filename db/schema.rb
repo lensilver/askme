2be7078df5ce21805af804caccda5ae7cd5846a5
lensilver <lensilverpr@hotmail.com>
2021-12-20 14:01:51 +0000

    Add background

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_05_192724) do
+ActiveRecord::Schema.define(version: 2021_12_20_130105) do
 
   create_table "questions", force: :cascade do |t|
     t.string "text"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2021_12_05_192724) do
     t.string "password_hash"
     t.string "password_salt"
     t.string "avatar_url"
+    t.string "color", default: "#005a55"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
